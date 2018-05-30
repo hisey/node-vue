4ab5884c75e306ac31a0d76623198f81bb748e0f
hisey <shijianxiang@mmtvip.com>
2018-05-30 17:45:43 +0800

    bug

---
+++ front-end2/src/views/goodsManage/goodsList.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="app-container">
+    <el-table :data="list" v-loading.body="listLoading" element-loading-text="Loading" border fit highlight-current-row>
+      <el-table-column align="center" label='ID' width="95">
+        <template slot-scope="scope">
+          {{scope.$index}}
+        </template>
+      </el-table-column>
+      <el-table-column label="名称">
+        <template slot-scope="scope">
+          {{scope.row.goodsList}}
+        </template>
+      </el-table-column>
+      <el-table-column label="商品分类" width="110" align="center">
+        <template slot-scope="scope">
+          <span>{{scope.row.category}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="价格" width="110" align="center">
+        <template slot-scope="scope">
+          {{scope.row.price}}
+        </template>
+      </el-table-column>
+      <el-table-column label="图片" width="110" align="center">
+        <template slot-scope="scope">
+           <img :src="scope.row.imgs" alt="">
+        </template>
+      </el-table-column>
+      <el-table-column label="创建时间" width="110" align="center">
+        <template slot-scope="scope">
+          {{scope.row.create_time}}
+        </template>
+      </el-table-column>
+      <el-table-column class-name="status-col" label="是否上架" width="110" align="center">
+        <template slot-scope="scope">
+          <el-tag :type="scope.row.status | statusFilter">{{scope.row.status}}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
+        <template slot-scope="scope">
+          <i class="el-icon-time"></i>
+          <span>{{scope.row.display_time}}</span>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+// import { getList } from '@/api/table'
+
+export default {
+  data() {
+    return {
+      list: null,
+      listLoading: true
+    }
+  },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        1: 'success',
+        0: 'gray',
+      }
+      return statusMap[status]
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.listLoading = true
+      // getList(this.listQuery).then(response => {
+      //   this.list = response.data.items
+      //   this.listLoading = false
+      // })
+    }
+  }
+}
+</script>
