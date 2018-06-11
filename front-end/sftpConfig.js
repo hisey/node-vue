144f23c4193cf17d4ed6c5548e9309d739ac9bd5
hisey <shijianxiang@mmtvip.com>
2018-06-11 08:58:55 +0800

    bug

--- front-end/sftpConfig.js
+++ front-end/sftpConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
       host: '193.112.202.42',
       user: 'root',
-      pass: 'v2DAugvhzXYLjD',
+      pass: '',
       port: "22",
       remotePath: "/var/hisey/app/front-end/dist"
 };
